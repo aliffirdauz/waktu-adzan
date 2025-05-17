9ff9d54948494727ff260061949c49cf6bd6296f
Muhammad Alif Firdaus <alifmuhammadfirdaus1@gmail.com>
2025-05-17 19:35:50 +0700

    change manifest file reference

--- app/_document.tsx
+++ app/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
     return (
         <Html lang="id">
             <Head>
-                <link rel="manifest" href="/manifest.webmanifest" />
+                <link rel="manifest" href="/manifest.json" />
                 <meta name="theme-color" content="#1e3a8a" />
 
                 <link rel="icon" type="image/png" sizes="192x192" href="/icons/android-icon-192x192.png"></link>
