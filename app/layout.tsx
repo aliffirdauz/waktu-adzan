ed1496463054c8eadaf687b9aee14257316f8e99
Muhammad Alif Firdaus <alifmuhammadfirdaus1@gmail.com>
2025-05-18 00:38:43 +0700

    add GlobalErrorCatcher

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
-'use client'
 import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import { useEffect, useState } from 'react'
+import GlobalErrorCatcher from '@/components/GlobalErrorCatcher'
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -19,29 +18,12 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
-
-  const [errorLog, setErrorLog] = useState<string | null>(null)
-
-  useEffect(() => {
-    window.addEventListener('error', (e) => {
-      setErrorLog(`🔥 Uncaught error: ${e.message}`)
-    })
-
-    window.addEventListener('unhandledrejection', (e) => {
-      setErrorLog(`🔥 Unhandled Promise rejection: ${e.reason}`)
-    })
-  }, [])
-
   return (
     <html lang="id" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
-          {errorLog && (
-            <div className="fixed bottom-0 left-0 right-0 bg-red-700 text-white text-xs p-2 font-mono z-50">
-              {errorLog}
-            </div>
-          )}
+          <GlobalErrorCatcher />
         </ThemeProvider>
       </body>
     </html>
